ede2374b173ce24dc13c1ee8863c26ea4135be2c
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-04-01 15:17:48 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const apiRoutes = require("./routes/apiRoutes");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,7 +17,7 @@ apiRoutes(app);
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
